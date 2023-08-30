bd138d14d858d806f459f4084e11993f67a21d96
learnandearnwithkarthik <125831794+learnandearnwithkarthik@users.noreply.github.com>
2023-08-30 14:44:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,90 +1,51 @@
-# Don't Edit
-
 import os
 from pyshorteners import Shortener
 from pyrogram import Client, filters
-import asyncio
-import datetime
-import logging
-import logging.config
-import sys
-from pyrogram import *
-from pyrogram.errors.exceptions.not_acceptable_406 import *
-from config import *
-from database import *
-from database.users import *
-from helpers import *
-from pyshorteners import *
-logging.config.fileConfig('logging.conf')
-logging.getLogger().setLevel(logging.INFO)
-import os
-import pyrogram
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
 
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) Shrimadhav U K
+# ...
 
+# Define your default shortener domain
+DEFAULT_SHORTENER_DOMAIN = "pdisk.site"
 
-if __name__ == "__main__" :
+# Define your other environment variables here
 
-    plugins = dict(
-        root="plugins"
-    )
-    dkbotz = Client(
-        "Mdisk-Pro",
-        bot_token=BOT_TOKEN,
-        api_id=API_ID,
-        api_hash=API_HASH,
-      def update_default_shortener_domain(new_domain):
-    os.environ["DEFAULT_SHORTENER_DOMAIN"] = new_domain,
-        plugins=plugins
-    )
-    
-    async def start(self):
-        me = await self.get_me()
-        self.owner = await self.get_users(int(OWNER_ID))
-        self.username = f'@{me.username}'
-        temp.BOT_USERNAME = me.username
-        temp.FIRST_NAME = me.first_name
-        if not await db.get_bot_stats():
-            await db.create_stats()
-        banned_users = await filter_users({"banned": True})
-        async for user in banned_users:
-            temp.BANNED_USERS.append(user["user_id"])
-        logging.info(LOG_STR)
-        await broadcast_admins(self, '** Bot started successfully **\n\nBot By @DKBOTZ')
-        logging.info('Bot started')
+# ...
 
+# Create your Client instance
+dkbotz = Client(
+    "Mdisk-Pro",
+    bot_token=BOT_TOKEN,
+    api_id=API_ID,
+    api_hash=API_HASH,
+    plugins={"root": "plugins"}
+)
 
+# Function to update the default shortener domain
+def update_default_shortener_domain(new_domain):
+    global DEFAULT_SHORTENER_DOMAIN
+    DEFAULT_SHORTENER_DOMAIN = new_domain
+
+# Handle the "/setdomain" command
+@dkbotz.on_message(filters.command("setdomain"))
+async def set_default_domain(_, message):
+    if len(message.command) != 2:
+        await message.reply("Usage: /setdomain [domain]")
+        return
+
+    new_domain = message.command[1]
+    supported_domains = ["pdisk.site", "moneycase.link", "streaan.in/T/"]  # Add more if needed
+    if new_domain in supported_domains:
+        update_default_shortener_domain(new_domain)
+        await message.reply(f"Default shortener domain set to {new_domain}")
+    else:
+        await message.reply("Invalid domain. Supported domains: " + ", ".join(supported_domains))
+
+# ...
+
+# Your start function and other bot logic here
+
+# ...
+
+# Run your bot
+if __name__ == "__main__":
     dkbotz.run()
-
-# Removed Upper All Codes Because This is Not Required Now. 
-
-#SESSION = "DKBOTZ"
-
-#class Bot(Client):
-
-    #def __init__(self):
-        #super().__init__(
-           # name=SESSION,
-            #api_id=API_ID,
-           # api_hash=API_HASH,
-           # bot_token=BOT_TOKEN,
-            #workers=50,
-           # plugins={"root": "plugins"},
-            #sleep_threshold=5,
-        #)
-
-
-
-    async def stop(self, *args):
-        await super().stop()
-        logging.info("Bot stopped. Bye.")
-
-#dkbotz = Bot()
-#dkbotz.run()
